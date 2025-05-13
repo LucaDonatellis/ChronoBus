d925fcfdf0e1086e90ab07643573426ad0526415
Luca <lucadonatellis03@gmail.com>
2025-05-13 13:57:31 +0200

    chore: added the base element for routing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,36 @@
 <script>
 	import '../app.css';
+	import { TriangleAlert,LayoutDashboard,User,ChartLine,MailWarning } from '@lucide/svelte';
+	import { page } from '$app/stores';
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+	{@render children()}
+	<div class="fixed bottom-18 right-4 bg-primary rounded-full w-12 h-12 flex items-center justify-center">
+
+		<div class="indicator ">
+			<span class="indicator-item indicator-bottom badge badge-secondary badge-xs">1</span>
+			<TriangleAlert/>
+		</div>
+	</div>
+<div class="dock dock-md">
+  <a class:dock-active={$page.url.pathname === '/dashboard'} href="/dashboard">
+    <LayoutDashboard />
+    <span class="dock-label">Dashboard</span>
+  </a>
+  
+  <a class:dock-active={$page.url.pathname === '/profile'} href="/profile">
+    <User />
+	<span class="dock-label">Profilo</span>
+  </a>
+  
+  <a class:dock-active={$page.url.pathname === '/rush-hours'} href="/rush-hours">
+    <ChartLine />
+	<span class="dock-label">Orari di punta</span>
+  </a>
+  <a class:dock-active={$page.url.pathname === '/report'} href="/report">
+    <MailWarning />
+	<span class="dock-label">Segnala</span>
+  </a>
+</div>
---
+++ src/routes/dashboard/Map.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { Search } from '@lucide/svelte';
+</script>
+<div class="fixed p-2 w-full">
+    <label class="input w-full">
+        <Search/>
+        <input type="search" required placeholder="Search" class="w-full rounded-b-fulll "/>
+    </label>
+</div>
