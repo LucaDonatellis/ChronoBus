9bf3f1161aa01adee2adced4092c854de71bba88
Luca <lucadonatellis03@gmail.com>
2025-05-28 23:28:39 +0200

    feat: enhance API endpoints for stops and trips, add arrival time for nearest stops

--- src/routes/API/v2/trentino-trasporti/trips_new/+server.js
+++ src/routes/API/v2/trentino-trasporti/trips_new/+server.js
@@ -4,21 +4,27 @@ import { json } from '@sveltejs/kit';
 import {
     TRENTINO_TRASPORTI_API_URL,
     TRENTINO_TRASPORTI_API_USERNAME,
-    TRENTINO_TRASPORTI_API_PASSWORD
+    TRENTINO_TRASPORTI_API_PASSWORD,
+    ALLOWED_ORIGIN
 } from '$env/static/private';
 
-const ALLOWED_ORIGIN = 'http://localhost:5173';
-
-export async function GET({request}) {
+export async function GET({request,url}) {
     const origin = request.headers.get('origin');    
 
     if (origin && origin !== ALLOWED_ORIGIN) {
         return new Response('Forbidden', { status: 403 });
     }
 
+    const stopId = parseFloat(url.searchParams.get('stopId'));
+	const limit = parseInt(url.searchParams.get('limit') || '10');
+
+	if (isNaN(stopId)) {
+		return json({ error: 'stopId required' }, { status: 400 });
+	}
+
     const auth = btoa(`${TRENTINO_TRASPORTI_API_USERNAME}:${TRENTINO_TRASPORTI_API_PASSWORD}`);
 
-    const res = await fetch(`${TRENTINO_TRASPORTI_API_URL}/trips_new?routeId=466&type=U&limit=3`, {
+    const res = await fetch(`${TRENTINO_TRASPORTI_API_URL}/trips_new?stopId=${stopId}&type=U&limit=${limit}`, {
         method: 'GET',
         headers: {
             Authorization: `Basic ${auth}`,
